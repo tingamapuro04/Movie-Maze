a9a1d0ff1a04a0016bc21895a6a412b8784242ad
tingamapuro04 <alponcemobutu100@gmail.com>
2022-06-29 23:30:05 +0300

    post comment mock

--- src/index.js
+++ src/index.js
@@ -6,4 +6,28 @@ import likeIt from './modules/likesAPI.js'
 
 window.addEventListener('DOMContentLoaded', render);
 
-likeIt()
+const body = document.querySelector("body");
+
+
+
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const appId = 'XbvsaAE4Kq5sfF9VGiHS';
+const commentUrl = `${url}${appId}/comments`;
+
+const mobutu = async (item_id) => {
+  const res = await fetch(commentUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: item_id,
+      'username': 'username',
+      'comment': 'comment',
+    }),
+  })
+  const data = res.text()
+  console.log(data)
+}
+
+mobutu(45)
---
+++ src/modules/commentPost.js
@@ -0,0 +1,3 @@
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const appId = 'XbvsaAE4Kq5sfF9VGiHS';
+const commentUrl = `${url}${appId}/comment/`;
