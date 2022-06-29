9125527ac26963ede28830224739e078ac29c421
tingamapuro04 <alponcemobutu100@gmail.com>
2022-06-29 12:38:51 +0300

    Linter corrections

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 import _ from 'lodash';
 import './style.css';
-import fetchFromApi from './modules/fromApi';
 import render from './modules/display';
 
-// const render = async () => {
-//   const mainCon = document.querySelector('#main-Con');
-//   const List = await fetchFromApi();
-//   List.slice(0, 9).forEach((movie) => {
-//     mainCon.innerHTML += `
-//     <div class="main" id="${List.indexOf(movie)}">
-//       <img src=${movie.image.medium} alt= ${movie.name}>
-//     </div>
-//     <div class="movieDetails">
-//       <h2>${movie.name}</h2>
-//       <i class="fa-light fa-heart"></i>
-//     </div>
-//     <div class="likes"><p><span></span>likes</p></div>
-//     <button class="Btn-comment">Comments</button>
-//     `;
-//   });
-//   const mobutu = document.querySelectorAll('.Btn-comment')
-//   console.log(mobutu);
-// };
-
 window.addEventListener('DOMContentLoaded', render)
-
-// const mobutu = document.querySelectorAll('.Btn-comment')
-// console.log(mobutu);
--- src/modules/display.js
+++ src/modules/display.js
@@ -3,16 +3,16 @@ import commentModal from "./Modal";
 
 
 const render = async() => {
-  const mainCon = document.querySelector('#main-Con');
+  const mainCon = document.querySelector('#main-Container');
   const List = await fetchFromApi()
-  List.slice(0, 9).forEach((movie) => {
+  List.slice(0, 20).forEach((movie) => {
     mainCon.innerHTML += `
     <div class="main" id="${List.indexOf(movie)}">
       <img src=${movie.image.medium} alt= ${movie.name}>
     </div>
     <div class="movieDetails">
       <h2>${movie.name}</h2>
-      <i class="fa-light fa-heart likes"></i>
+      <i class="fa-regular fa-heart"></i>
       <p class="likeCount"></p>
     </div>
     <button class="Btn-comment" id=${List.indexOf(movie)}>Comments</button>
