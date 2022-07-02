c55f6b2031f49da0a1de562d35a9a87e634d754f
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-02 11:32:45 +0300

    postlike

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import {render} from './modules/display.js';
 import { likePaticular } from './modules/display.js'
 
 window.addEventListener('DOMContentLoaded', render)
-const body = document.querySelector("body");
 const like = document.querySelector('body')
 like.addEventListener('click', () => {
   likePaticular()
 })
+
+
 
 
 
